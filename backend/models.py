720d2065312b30986b36aa18f0b6f4e651b57e7e
Elicupra <eliseomog1989@gmail.com>
2025-11-14 16:58:34 +0100

    nuevas clases. script de test

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from database import Base
+from datetime import datetime
 
 class Driver(Base):
     __tablename__ = "drivers"
@@ -8,10 +9,103 @@ class Driver(Base):
     team = Column(String)
     country = Column(String)
     points = Column(Float)
+    wins = Column(Integer)
+    podiums = Column(Integer)
+    pole_positions = Column(Integer)
+    fastest_laps = Column(Integer)
+    championships = Column(Integer)
+    grand_chelem = Column(Integer)
 
 class Team(Base):
     __tablename__ = "teams"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     country = Column(String)
+    year_founded = Column(String) #Formato YYYY
     points = Column(Float)
+    team_principal = Column(String)
+
+class GrandPrix(Base):
+    __tablename__ = "grand_prix"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    location = Column(String)
+    id_track_name = Column(Integer, ForeignKey("tracks.id"))
+    track_name = Column(String)
+    date_event = Column(String) #Formato DD/MM/YYYY to DD/MM/YYYY
+    fastest_lap = Column(String)
+    fastest_driver = Column(String)
+    fastest_lap_year = Column(String)
+    id_driver = Column(Integer, ForeignKey("drivers.id"))
+    id_team = Column(Integer, ForeignKey("teams.id"))
+    winner_driver = Column(String)
+    podium_2 = Column(String)
+    podium_3 = Column(String)
+    winner_team = Column(String)
+    poleman = Column(String)
+    pole_team = Column(String)
+    pole_time = Column(String) #Formato MM:SS:MS
+    qualification_result = Column(String) #Formato JSON
+    grid = Column(String) #Formato JSON
+    race_results = Column(String) #Formato JSON
+    date = Column(String)
+    weekend_sprint = Column(bool)
+    sprint_shotout_man = Column(String) #Piloto mas rapido en la sesion sprint shotout
+    sprint_shotout_lap = Column(String) #Tiempo del piloto mas rapido en la sesion sprint shotout
+    sprint_shotout_result = Column(String) #Formato JSON
+    sprint_grid = Column(String) #Formato JSON
+    sprint_winner = Column(String)
+    sprint_podium_2 = Column(String)
+    sprint_podium_3 = Column(String)
+    sprint_winner_team = Column(String)
+
+
+class Track(Base)   :
+    __tablename__ = "tracks"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    location = Column(String)
+    country = Column(String)
+    year = Column(String) #Formato YYYY
+    length_km = Column(Float)
+    number_of_laps = Column(Integer)
+    lap_record_time = Column(String) #Formato MM:SS:MS
+    lap_record_driver = Column(String)
+    lap_record_year = Column(String)
+
+class Car(Base):
+    __tablename__ = "cars"
+    id = Column(Integer, primary_key=True, index=True)
+    id_team = Column(Integer, ForeignKey("teams.id"))
+    id_chasis = Column(Integer, ForeignKey("chassis.id"))
+    id_engine = Column(Integer, ForeignKey("engines.id"))
+    id_pneumatics = Column(Integer, ForeignKey("pneumatics.id"))
+    team = Column(String)
+    chasis = Column(String)
+    engine = Column(String)
+    weight_kg = Column(Float)
+    pneumatics = Column(String)
+    designer = Column(String) #Persona responsable del diseño del coche
+
+class Chasis(Base):
+    __tablename__ = "chassis"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    material = Column(String)
+    suspension_type = Column(String)
+    year_introduced = Column(String) #Formato YYYY
+    designer = Column(String) #Persona responsable del diseño del chasis
+
+class Engine(Base):
+    __tablename__ = "engines"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    type = Column(String) #Ejemplo: V6 Turbo Hybrid
+    angle_configuration = Column(String) #Ejemplo: 90 degrees
+    cylinders = Column(Integer)
+    aspiration = Column(String) #Ejemplo: Turbocharged
+    cooling_system = Column(String) #Ejemplo: Liquid-cooled
+    transmission = Column(String) #Ejemplo: 8-speed semi-automatic
+    type_transmission = Column(String) #Ejemplo: Sequential
+    transmission_axle = Column(String) #Ejemplo: Rear-wheel drive    manufacturer = Column(String)
+    year_introduced = Column(String) #Formato YYYY
---
+++ backend/test.py
@@ -0,0 +1,82 @@
+import requests
+import fastf1
+
+
+BASE = "https://api.jolpi.ca/ergast/f1/"
+
+
+def Winner_Last_Race():
+    url = f"{BASE}2025/last/results.json"
+    r = requests.get(url)
+    data = r.json()
+    Nombre_ganador = data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["Driver"]["givenName"]
+    Apellido_ganador = data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["Driver"]["familyName"]
+    Ganador = f"{Nombre_ganador} {Apellido_ganador}"
+    #Evento = data("MRData")("RaceTable")("Races")[0]  # Obtener el último gran premio
+    # Obtener el ganador de la carrera
+    return print(f"Ganador del ultimo gran Premio: {Ganador}")
+
+
+def Search_Position_Driver_last_Race(driver_name):
+    url = f"{BASE}2025/last/results.json"
+    r = requests.get(url)
+    data = r.json()
+    #Driver = data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["Driver"]["givenName"] + " " + data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["Driver"]["familyName"]
+    #position = data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["position"]
+    #Fernando_Alonso = data["MRData"]["RaceTable"]["Races"][0]["Results"][13]["Driver"]["driverId"]
+
+    
+    i = data["MRData"]["RaceTable"]["Races"][0]["Results"][i]["Driver"]["givenName"] + " " + data["MRData"]["RaceTable"]["Races"][0]["Results"][i]["Driver"]["familyName"]
+    
+    for driver_name in i:
+        posicion = data["MRData"]["RaceTable"]["Races"][0]["Results"][i]["position"]
+        return posicion
+        
+      
+
+    
+    
+    
+    #id_driver =
+    #if Driver == driver_name:
+    #position = data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["position"]
+    #    for i in position in data["MRData"]["RaceTable"]["Races"][0]["Results"][i]
+            
+        
+        
+    #    return print(f"{driver_name} finalizo en la posicion: {position}")
+    #else:
+    #    return print(f"{driver_name} did not participate in the last race.")
+    
+    
+    
+
+    
+    
+
+
+#def get_driver_standings():
+#    url = f"{BASE}current/driverStandings.json"
+#    r = requests.get(url)
+#    data = r.json()
+#    return data["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]
+
+#standings = get_driver_standings()
+#for d in standings:
+#    print(d["Driver"]["givenName"], d["Driver"]["familyName"], d["points"])
+
+if __name__ == "__main__":
+         
+    Winner_Last_Race()
+
+    print("-----")
+   # print("¿Quieres saber la posicion de un piloto en la ultima carrera?")
+    #piloto = input("Introduce el nombre completo del piloto (Ejemplo: Max Verstappen): ")
+    Search_Position_Driver_last_Race("Fernando Alonso")
+
+
+    #clasificacion_pilotos = get_driver_standings()
+
+    #print(f"Driver Standings: {clasificacion_pilotos}")
+
+pass
